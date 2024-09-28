11cf43478fd7f87416962f5966c50f958c1ab146
Kazuki Sakamoto <sakzk.youronian@gmail.com>
2024-09-28 21:23:38 +0900

    数字・スペース以外の文字と、オーバーフローはケアしない実装

--- sources/forth_modoki/01_int_parser/int_parser.c
+++ sources/forth_modoki/01_int_parser/int_parser.c
@@ -3,6 +3,7 @@
 
 static const char* const input = "123 456  1203";
 
+const char* parse_num(const char* p, int* result);
 
 int main() {
     int answer1 = 0;
@@ -10,7 +11,10 @@ int main() {
     int answer3 = 0;
 
     // write something here.
-
+    const char* p = input;
+    p = parse_num(p, &answer1);
+    p = parse_num(p, &answer2);
+    p = parse_num(p, &answer3);
 
     // verity result.
     assert(answer1 == 123);
@@ -18,4 +22,15 @@ int main() {
     assert(answer3 == 1203);
 
     return 0;
+}
+
+const char* parse_num(const char* p, int* result) {
+    *result = 0;
+    while ('0' <= *p && *p <= '9') {
+        *result = 10 * (*result) + (*p - '0');
+        p++;
+    }
+    while (*p == ' ')
+        p++;
+    return p;
 }
